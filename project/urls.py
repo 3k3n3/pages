f3e8486cfa8409f50522a79449e2776a0d2ddaaa
Ekene C <clarencekene@outlook.com>
2023-03-20 02:54:03 +0100

    Added project level templates folder for html file Set up login & logout with django auth Set up signup page, views and urls and more tests Additional configs in settings.py for login/out redirect

--- project/urls.py
+++ project/urls.py
@@ -17,6 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    # django admin
     path("admin/", admin.site.urls),
+    # auth
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/", include("accounts.urls")),
+    # apps 
     path("", include("pages.urls")),
+
 ]
